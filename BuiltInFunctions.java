6ae19cb2c963a2515acee8d0b765b48016c4f131
deshawashington <washind14@icloud.com>
2022-05-27 15:57:19 -0600

    commit

---
+++ BuiltInFunctions.java
@@ -0,0 +1,14 @@
+public class BuiltInFunctions {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sine of 1.2 is "+sine);
+
+        double power = Math.pow(2, 4);
+        System.out.println("2 to the power of 4 is: "+ power);
+
+        double random = Math.random() * 10;
+        System.out.println("The random value is "+ random);
+    }
+}
